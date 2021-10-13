30ac90b343b85f144db40a57b42196dc3babda48
shubhamd25 <dubeyshubham902@gmail.com>
2021-10-13 06:06:25 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -3,21 +3,36 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+         <h1>-: Table using React :-</h1>
+      <table>
+        <thead>
+          <th>Name</th>
+          <th>Designation</th>
+          <th colspan="2">Adress</th>
+          
+        </thead>
+        <tbody>
+          <tr>
+            <td>Shubham</td>
+            <td>Full Stack</td>
+            <td>dubeyshubham902@gmail.com</td>
+          </tr>
+          <tr>
+            <td>Shubham</td>
+            <td>Full Stack</td>
+            <td>dubeyshubham902@gmail.com</td>
+          </tr>
+          <tr>
+            <td>Shubham</td>
+            <td>Full Stack</td>
+            <td>dubeyshubham902@gmail.com</td>
+          </tr>
+        </tbody>
+        <tfoot>
+          <th colspan="3">Performance gd</th>
+        </tfoot>
+      </table>
     </div>
   );
 }
